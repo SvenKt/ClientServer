8d70cf637fd595ebbf84bde841afc330535adcd8
Sven <you@example.com>
2016-03-16 12:41:58 +0100

    compile

--- compile.sh
+++ compile.sh
@@ -1,7 +1,9 @@
 
 cp InsertClient.cpp InsertClient.pgc;
 cp Wissensbereiche.cpp Wissensbereiche.pgc; 
+cp Fragen.cpp Fragen.pgc;
+cp Highscore.cpp Highscore.pgc
 ecpg *.pgc; 
-g++ -I /usr/include/postgresql -c InsertClient.c Wissensbereiche.c ;
+g++ -I /usr/include/postgresql -c InsertClient.c Wissensbereiche.c Fragen.c Highscore.c;
 g++ -o main *.o -L/usr/lib/psql -lecpg;
 ./main
